c20af4e925ba52cf94f23688fd776e7cfd109cb3
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-01-02 12:45:13 -0200

    Refactor grid system and header and main home

--- assets/js/nav.js
+++
@@ -1,8 +0,0 @@
-function responsiveTopNavigation(){
-  var x = document.getElementById("topNav");
-  if(x.className == "navClass" ){
-    x.className += " responsive";
-  }else{
-    x.className = "navClass"
-  }
-}
